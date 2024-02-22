8d576b7669fb24a4ff081075ef98b9169c216c62
rebeccamastrostefano <mastrostefanorebecca2@gmail.com>
2024-02-22 14:19:11 +0100

    add healing stone

---
+++ ForsakenLegacy/Assets/ForsakenLegacy/Scripts/General/HealingStone/HealingStone.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using ForsakenLegacy;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class HealingStone : MonoBehaviour
+{
+    public GameObject player;
+    public TMP_Text tutorial;
+    public GameObject blood;
+    private bool inInteractionArea;
+    public bool isActive = true;
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player") && isActive)
+        {
+            tutorial.text = "Press E to Collect";
+            inInteractionArea = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (inInteractionArea && isActive && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            player.GetComponent<HealthSystem>().IncreasePotions(2);
+            isActive = false;
+            blood.SetActive(false);
+            tutorial.text = "";
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player") && isActive)
+        {
+            tutorial.text = "";
+        }
+    }
+}
--- ForsakenLegacy/Assets/ForsakenLegacy/Scripts/Player/HealthSystem.cs
+++ ForsakenLegacy/Assets/ForsakenLegacy/Scripts/Player/HealthSystem.cs
@@ -13,9 +13,12 @@ namespace ForsakenLegacy
         private bool isInvulnerable = false;
         private float timeSinceLastHit = 0.0f;
         public float invulnerabiltyTime = 1.0f; // The time in seconds the player is invulnerable after taking damage.
+
+        public int healingPotions;
         
         // References to UI elements
         public Image healthBarFill; // Assign this in the inspector
+        public TMP_Text potionNumberText; // Assign this in the inspector
 
         //Feedbacks
         public MMFeedbacks hitFeedback; // Assign this in the inspector
@@ -85,6 +88,12 @@ namespace ForsakenLegacy
 
         public void Heal(int healingAmount)
         {
+            if(healingPotions <= 0)
+            {
+                return;
+            }
+
+            healingPotions -= 1; // Decrement the number of healing potions by 1.
             currentHealth += healingAmount;
             if (currentHealth > maxHealth)
             {
@@ -92,6 +101,13 @@ namespace ForsakenLegacy
             }
             
             UpdateHealthUI();
+            UpdatePotionUI();
+        }
+
+        public void IncreasePotions(int potions)
+        {
+            healingPotions += potions;
+            UpdatePotionUI(); // Update the UI to reflect the new number of healing potions.
         }
 
         private void Die()
@@ -99,6 +115,10 @@ namespace ForsakenLegacy
             Debug.Log("Player Died");
             deathFeedback.PlayFeedbacks();
             // Implement what happens when the player dies.
+        }
+        public void UpdatePotionUI()
+        {
+            potionNumberText.text = healingPotions.ToString(); // Update the UI text with the new number of healing potions.
         }
 
         private void UpdateHealthUI()
